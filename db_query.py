19455e74611380ed9454a4476a3da37652e0bcc5
venkat <37570179+venkatn7@users.noreply.github.com>
2020-04-20 22:51:42 -0500

    Add files via upload
    Note: To run files you need to setup and have access to SQL database.

---
+++ db_query.py
@@ -0,0 +1,13 @@
+from main import User
+from flask import jsonify
+
+x = User.query.all()
+y = User.query.filter_by(username='ven_123').first()
+z = User.query.get(31)
+print(jsonify(z))
+
+
+# def qrest():
+#     with app.app_context():
+#         users = User.query.all()
+#         print(users)
---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/pyapilab'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return '<User %r>' % self.id + '\n' + self.username + '\n' + self.email
+
